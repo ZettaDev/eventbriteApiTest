768282e09af734087e575bf2293341ae29ac6a1d
Hideki <junichi.harem@gmail.com>
2015-05-08 10:12:39 +0100

    Filtrado por class div

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-var eventBriteToken = "MQRI5BWCXONRZZOQDKL3";
+var eventBriteToken = "";
 var eventBriteOrganizerId = "8135119990";
 // config general de la app
 
@@ -10,45 +10,47 @@ $(document).ready( function() {
 
 function llamadaApi(e) {
     //var urlApi = "https://www.eventbriteapi.com/v3/events/search/?organizer.id="+eventBriteOrganizerId+"&token="+eventBriteToken;
-    var urlApi = "data.json";
+    var urlApi = "http://localhost/fct/eventbriteApiTest/js/data.json";
     //alert(urlApi);
+
+    var htmlDatosTipo1 = "";
+    var htmlDatosTipo2 = "";
+    var htmlDatosTipo3 = "";
 
     $.get( urlApi , function( data ) {
         //$( "#result" ).html( JSON.stringify( data ) );
         //alert( "Load was performed." );
-        var htmlInsertar = "";
+
         $.each(data.events,function(i, evento){
-            htmlInsertar += "<div class='row'>";
-            htmlInsertar += "<div class='col-md-8'>";
-            htmlInsertar += "<p>"+evento.name.html+"</p><br />";
-            htmlInsertar += "<p>Empieza el: "+evento.start.local+" Acaba el: "+evento.end.local+"</p></div>";
-            //htmlInsertar += "<div class='col-md-4'><img src='"+evento.logo.url+"'></div>";
-            htmlInsertar += "</div>";
-            htmlInsertar += "<div class='row'>";
-            htmlInsertar += "<div class='col-md-8'><div class='panel panel-default'><div class='panel-heading'>";
-            htmlInsertar += "<h3 class='panel-title'>Información de la entrada</h3>";
-            htmlInsertar += "</div><div class='panel-body panel-primary'>";
-            htmlInsertar += "<table class='table table-striped'><tr><td><strong>TIPO DE ENTRADA</strong></td><td><strong>DESCRIPCION</strong></td><td><strong>PRECIO</strong></td></tr>";
+            var htmlOut = "<h1>titulo</h1><br />";
+            htmlOut += "<p>"+evento.name.html+"</p><br /><br />";
+            htmlOut += "<h1>localizacion</h1><br />";
+            htmlOut += "<p>"+evento.venue.name+" "+evento.venue.address.address_1+" "+evento.venue.address.region+"</p><br /><br />";
+            htmlOut += "<h1>entradas</h1><br />";
             $.each(evento.ticket_classes,function(i, precio){
-                htmlInsertar += "<tr>";
-                htmlInsertar += "<td>"+precio.name+"</td>";
-                htmlInsertar += "<td>"+precio.description+"</td>";
-                //htmlInsertar += "<td>"+precio.cost.display+"</td>";
-                htmlInsertar += "</tr>";
+                var htmlTabla = "<table class='table table-striped'><tr><td><strong>TIPO DE ENTRADA</strong></td><td><strong>DESCRIPCION</strong></td><td><strong>PRECIO</strong></td></tr>";
+                htmlTabla += "<tr>";
+                htmlTabla += "<td>"+precio.name+"</td>";
+                htmlTabla += "<td>"+precio.description+"</td>";
+                htmlTabla += "<td>"+precio.cost.display+"</td>";
+                htmlTabla += "</tr>";
+                htmlTabla += "</table>";
+                htmlOut += "<p>"+htmlTabla+"</p><br /><br />";
             });
-            htmlInsertar += "</table>";
-            htmlInsertar += "</div></div></div>";
-            //htmlInsertar += "<div class='col-md-4'>"+evento.organizer.description.html+"</div>";
-            htmlInsertar += "</div>";
-            htmlInsertar += "<div class='col-md-12'><div class='panel panel-default'><div class='panel-heading'>";
-            htmlInsertar += "<h3 class='panel-title'>Detalles del evento</h3>";
-            htmlInsertar += "</div><div class='panel-body'>";
-            htmlInsertar += evento.description.html;
-            htmlInsertar += "</div></div>";
-            htmlInsertar += "</div>";
+            htmlOut += "<h1>detalle evento</h1><br />";
+            var edesc = evento.description.html;
+            htmlOut += "<p>"+edesc+"</p><br /><br />";
+            if (edesc.indexOf("<div class='tipo1") == 0){
+                htmlDatosTipo1 += htmlOut;
+            } else if (edesc.indexOf("<div class='tipo2") == 0){
+                htmlDatosTipo2 += htmlOut;
+            } else if (edesc.indexOf("<div class='tipo3") == 0){
+                htmlDatosTipo3 += htmlOut;
+            }
         });
-        $( "#result" ).html( htmlInsertar );
-        alert( "Load was performed." );
+       $( "#tipo1" ).html( htmlDatosTipo1 );
+       $( "#tipo2" ).html( htmlDatosTipo2 );
+       $( "#tipo3" ).html( htmlDatosTipo3 );
     });
 
 }
